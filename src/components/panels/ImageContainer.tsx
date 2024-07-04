2c3b4e76d021f3c4bb45f41e973ec9c0cc166c9a
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-07-04 16:13:09 +0100

    Update image component for different screen sizes

--- src/components/panels/ImageContainer.tsx
+++ src/components/panels/ImageContainer.tsx
@@ -1,28 +1,39 @@
 'use client';
+import useMediaQuery from '@/hooks/useMediaQuery';
 import { ImageData } from '@/types/homePageTypes';
 import Image from 'next/image';
 
 interface ImageContainerProps {
   image: ImageData;
-  widthOffset: number;
-  heightOffset: number;
 }
 
-const ImageContainer: React.FC<ImageContainerProps> = ({
-  image,
-  widthOffset,
-  heightOffset,
-}) => {
+const ImageContainer: React.FC<ImageContainerProps> = ({ image }) => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
+  const largeScreenBreakpoint = useMediaQuery(1025);
+  const xlScreenBreakpoint = useMediaQuery(1201);
+  const getStyle = () => {
+    if (largeScreenBreakpoint) {
+      return {
+        height: `${image.attributes.formats.small.height - 325}px`,
+        width: `${image.attributes.formats.small.width - 325}px`,
+      };
+    } else if (xlScreenBreakpoint) {
+      return {
+        height: `${image.attributes.formats.medium.height - 550}px`,
+        width: `${image.attributes.formats.medium.width - 550}px`,
+      };
+    } else {
+      return {
+        height: `${image.attributes.formats.medium.height - 550}px`,
+        width: `${image.attributes.formats.medium.width - 550}px`,
+      };
+    }
+  };
   return (
     <>
       <div
         className='relative rounded-lg 2xl:h-[200px] 2xl:w-[200px]'
-        style={{
-          height: `${image.attributes.formats.small.height - heightOffset}px`,
-          width: `${image.attributes.formats.small.width - widthOffset}px`,
-        }}
+        style={getStyle()}
       >
         <Image
           src={apiUrl + image.attributes.formats.small.url}
