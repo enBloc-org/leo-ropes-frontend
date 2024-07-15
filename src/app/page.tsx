31ce0418157889c74edd636c5d3bb21d79f96fee
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-07-15 17:18:40 +0100

    Main page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,15 @@ export default async function Home() {
   const data: HomePageAttributes = response.data.attributes;
 
   return (
-    <MainGrid>
+    <MainGrid className='relative top-20 pb-10 lg:top-[117px]'>
       <h1
-        className='col-span-full mb-5 mt-10 px-6 text-center text-3xl leading-9
+        className='col-span-full mb-5 mt-10 px-6 text-center text-3xl leading-9 lg:mb-0
        lg:text-6xl'
       >
         {data.headerText}...
       </h1>
       <div
-        className='col-span-full m-auto mt-2 grid grid-cols-2 items-center justify-center gap-2
+        className='col-span-full m-auto grid grid-cols-2 items-center justify-center gap-2
         md:mt-8 md:grid-cols-3 lg:col-start-2 lg:col-end-7'
       >
         {data.images.data.map((image) => {
@@ -42,7 +42,7 @@ export default async function Home() {
       </div>
       <div
         className='xl:text-xl col-span-full mt-10 flex flex-col items-center justify-around px-6
-        sm:px-12 md:col-start-2 md:col-end-6 lg:col-start-7 lg:col-end-12 lg:mt-8 lg:text-lg '
+        sm:px-12 md:col-start-2 md:col-end-6 lg:col-start-7 lg:col-end-12 lg:mt-8 lg:pb-20 lg:text-lg '
       >
         <RichTextEditor editorContent={data.heroText} />
         <p className='mt-6 text-center font-semibold'>{data.tagLine}</p>
