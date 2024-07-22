db26f1a3311689c38ea613b877886929b7f40dc2
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-07-22 12:00:39 +0100

    Type fix, default array value and uneccessary import statments and vairalbles

--- src/utils/fetchStrapiContent.ts
+++ src/utils/fetchStrapiContent.ts
@@ -1,15 +1,12 @@
-import { flattenAttributes } from './flattenAttributes';
+import { SinglePageResponse } from '@/types/singlePageTypes';
 
 export async function fetchStrapiContent(endPoint: string) {
   try {
-    const res = await fetch(
-      `${process.env.STRAPI_API_URL}/${endPoint}`,
-      {
-        next: { revalidate: 0 },
-      }
-    );
+    const res = await fetch(`${process.env.STRAPI_API_URL}/${endPoint}`, {
+      next: { revalidate: 0 },
+    });
     const data = await res.json();
-    return data;
+    return data ? data : [];
   } catch (error) {
     console.error(error);
   }
