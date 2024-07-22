20053a208f51a74d2605e7fb3f690ed61ec09530
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-07-22 15:13:07 +0100

    Next config fix for images

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'leo-ropes-space.ams3.digitaloceanspaces.com',
-        pathname: '/uploads/**',
+        port: ''
       },
     ],
   },
